32f57e938a8e4fc473bd63986083cf41ca8e760d
regis <regis.aubarede@gmail.com>
2014-08-17 16:02:45 +0200

    license

--- samples/proxy.rb
+++ samples/proxy.rb
@@ -1,21 +1,17 @@
+#!/usr/bin/ruby
+# LGPL
 ####################################################
 # proxy.rb : pure tcp proxy, for dev/admin/fun :)
+#
+# Usage :
+#   > ruby proxy.rb target-hostname target-port [mode]
+#       mode= ascii/bin/none : style of printing
 ####################################################
-require_relative '../minitcp.rb'
+require_relative '../lib/minitcp.rb'
 
-def spy(sens,data)
-  return if $opt=="none"
-  prefix=(sens==1 ? "< " : "> ")
-  if $opt=="ascii"
-    data.each_line {|line| puts "#{prefix}#{line.chomp}"  }
-  else
-    data.chars.each_slice(16) do |aline|
-      a=(aline.map { |char| "%02X " % char.ord }).join.ljust(16*3)+pref+
-         aline.map { |char| (char.ord>30 ? char : "~") }.join()
-      puts "#{prefix}#{a}"
-    end
-  end
-end
+$host,$port,$opt=ARGV[0]||"localhost",ARGV[1]||80,ARGV[2]||"ascii"
+puts "Server on port 2200, proxy to #{$host}:#{$port}..."
+
 
 MServer.service(2200,"0.0.0.0",22) do |s_cli|
   puts "> ======== client Connected ========"
@@ -31,11 +27,20 @@ MServer.service(2200,"0.0.0.0",22) do |s_cli|
   srv.kill
 end
 
+def spy(sens,data)
+  return if $opt=="none"
+  prefix=(sens==1 ? "< " : "> ")
+  if $opt=="ascii"
+    data.each_line {|line| puts "#{prefix}#{line.chomp}"  }
+  else
+    data.chars.each_slice(16) do |aline|
+      a=(aline.map { |char| "%02X " % char.ord }).join.ljust(16*3)+pref+
+         aline.map { |char| (char.ord>30 ? char : "~") }.join()
+      puts "#{prefix}#{a}"
+    end
+  end
+end
 
-if $0==__FILE__
+sleep
 
-  $host,$port,$opt=ARGV[0]||"localhost",ARGV[1]||80,ARGV[2]||"ascii"
-  puts "Server on port 2200, proxy to #{$host}:#{$port}..."
-  sleep
-end  
 
