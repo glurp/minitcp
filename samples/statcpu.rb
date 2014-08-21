e10cecdfb4c6e9bbd8f32d24fbe38bcb7078cb65
regis <regis.aubarede@gmail.com>
2014-08-21 11:05:30 +0200

    creation

---
+++ samples/statcpu.rb
@@ -0,0 +1,29 @@
+# LGPL
+# ruby b.rb  0.2  3 | ruby plot.rb --pos 300,0  0 -0.01 1 cpu
+
+def get_nbcpu()
+	stat = Hash[*(IO.read("/proc/stat").split("\n").map {|line| line.chomp.split(/\s+/,2)}.flatten)]
+	stat.keys.grep(/^cpu(\d+)/).size
+end
+def get_stat()
+	stat = Hash[*(IO.read("/proc/stat").split("\n").map {|line| line.chomp.split(/\s+/,2)}.flatten)]
+	p stat if $DEBUG
+	stat=stat["cpu"].split(/\s+/).map(&:to_i)
+end
+
+
+$stdout.sync=true
+periode= ARGV.shift.to_f
+lno=ARGV.map(&:to_i)
+p periode
+p lno
+nbcpu=get_nbcpu()
+p nbcpu
+ref=get_stat()
+loop {
+		ref=get_stat().tap {|t|
+			x=t.zip(ref).map {|a,b| ((a-b)*10)/(nbcpu*periode*1000)} 
+		    puts lno.map {|i| (100*x[i]).to_i}.join(" ")
+	    }
+		sleep periode
+}
