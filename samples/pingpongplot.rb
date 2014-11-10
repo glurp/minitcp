a2033d2e10d69511c5008ec125f5fc587f216171
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-11-10 18:51:04 +0100

    multiple url test

--- samples/pingpongplot.rb
+++ samples/pingpongplot.rb
@@ -9,18 +9,24 @@ require_relative '../lib/minitcp.rb'
 
 
 $stdout.sync=true
-
-MClient.run_continious("google.com",80,50) do |socket|
-  s=Time.now.to_f
-  socket.on_receive_sep("\r\n") do |data| 
-  	$stdout.puts("#{(Time.now.to_f-s)*1000}") 
-  	$stdout.flush 
-  	socket.close rescue nil
+l=ARGV.size>0 ? ARGV : ["google.com"]
+$data=(1..l.size).map {|| "0"}
+l.each_with_index do |url,index|
+  MClient.run_continious(url,80,1000-100) do |socket|
+    s=Time.now.to_f
+    socket.on_receive_sep("\r\n") do |data| 
+      $data[index]="#{((Time.now.to_f-s)*1000).round}"
+      socket.close rescue nil
+    end
+    s=Time.now.to_f
+    socket.print "GET /blabla HTTP/1.0\r\nHost: ici\r\n\r\n"
+    socket.wait_end
   end
-  s=Time.now.to_f
-  socket.print "GET /blabla HTTP/1.0\r\nHost: ici\r\n\r\n"
-  socket.wait_end
 end
-
-
+Thread.new {|| loop {
+  data=$data.clone
+  $stdout.puts data.join(" ")
+  $stdout.flush
+  sleep 1
+}}
 sleep
